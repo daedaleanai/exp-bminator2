50577bc542cb9e84fd582bd69e218e80f7d266a7
Luuk van Dijk <lvd@daedalean.ai>
2023-10-08 16:22:16 +0200

    made usart entirely inline

--- usart.h
+++ usart.h
@@ -12,11 +12,27 @@
 //
 // Before calling usart_init, make sure to set up the GPIO pins: TX to AF_PP/10MHz. RX to IN FLOATING or Pull-up.
 // and to enable the USART in the RCC register:	RCC->APBxENR |= RCC_APBxENR_USARTyEN;
-void usart_init(struct USART1_Type* usart, int baud);
+inline void usart_init(struct USART1_Type* usart, int baud) {
+	usart->CR1 = 0; 
+	usart->CR2 = 0;
+	usart->CR3 = 0;
+	usart->BRR = (80000000 + baud/2) / baud;
+	usart->CR1 |= USART1_CR1_UE | USART1_CR1_TE;
+}
+
 
 // Create IRQ Handlers for the usarts you use like so:
 // 	void USARTx_IRQ_Handler(void) { usart_irq_andler(&USARTx, txbufx); }
-void usart_irq_handler(struct USART1_Type* usart, struct Ringbuffer* rb);
+inline void usart_irq_handler(struct USART1_Type* usart, struct Ringbuffer* rb) {
+	if (!ringbuffer_empty(rb)) {
+		if ((usart->ISR & USART1_ISR_TXE) != 0) {
+			usart->TDR = ringbuffer_get_tail(rb);   
+		}
+	} else {
+		usart->CR1 &= ~USART1_CR1_TXEIE;
+	}
+	return;
+}
 
 // start transmission
 inline void usart_start(struct USART1_Type* usart) { usart->CR1 |= USART1_CR1_TXEIE;  }
