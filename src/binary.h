cb3a5279aec996e64592b82c4dadf6f3916c5925
Luuk van Dijk <lvd@daedalean.ai>
2023-10-28 05:57:40 +0200

    complete big/little decode/encode

--- src/binary.h
+++ src/binary.h
@@ -12,14 +12,30 @@ inline uint32_t decode_le_uint24(uint8_t *buf) {
 }
 
 inline uint32_t decode_le_uint32(uint8_t *buf) {
-    return ((uint32_t)(buf[0])) | (((uint32_t)(buf[1])) << 8) | (((uint32_t)(buf[2])) << 16) |
-            (((uint32_t)(buf[3])) << 24);
+    return ((uint32_t)(buf[0])) | (((uint32_t)(buf[1])) << 8) | (((uint32_t)(buf[2])) << 16) | (((uint32_t)(buf[3])) << 24);
 }
 
 inline uint64_t decode_le_uint64(uint8_t *buf) {
-    return ((uint64_t)(buf[0])) | (((uint64_t)(buf[1])) << 8) | (((uint64_t)(buf[2])) << 16) |
-            (((uint64_t)(buf[3])) << 24) | (((uint64_t)(buf[4])) << 32) | (((uint64_t)(buf[5])) << 40) |
-            (((uint64_t)(buf[6])) << 48) | (((uint64_t)(buf[7])) << 56);
+    return ((uint64_t)(buf[0])) | (((uint64_t)(buf[1])) << 8) | (((uint64_t)(buf[2])) << 16) | (((uint64_t)(buf[3])) << 24) 
+         | (((uint64_t)(buf[4])) << 32) | (((uint64_t)(buf[5])) << 40) | (((uint64_t)(buf[6])) << 48) | (((uint64_t)(buf[7])) << 56);
+}
+
+// big endian binary decoding
+inline uint16_t decode_be_uint16(uint8_t *buf) {
+    return ((uint16_t)(buf[0]) << 8) | ((uint16_t)(buf[1]));
+}
+
+inline uint32_t decode_be_uint24(uint8_t *buf) {
+    return (((uint32_t)(buf[0])) << 16) | (((uint32_t)(buf[1])) << 8) | ((uint32_t)(buf[2]));
+}
+
+inline uint32_t decode_be_uint32(uint8_t *buf) {
+    return ((uint32_t)(buf[0]) << 24) | (((uint32_t)(buf[1])) << 16) | (((uint32_t)(buf[2])) << 8) | ((uint32_t)(buf[3]));
+}
+
+inline uint64_t decode_be_uint64(uint8_t *buf) {
+    return ((uint64_t)(buf[0]) << 56) | (((uint64_t)(buf[1])) << 48) | (((uint64_t)(buf[2])) << 40) | (((uint64_t)(buf[3])) << 32) 
+         | ((uint64_t)(buf[4]) << 24) | (((uint64_t)(buf[5])) << 16) | (((uint64_t)(buf[6])) << 8) | ((uint64_t)(buf[7]));
 }
 
 // little endian binary encoding
@@ -50,27 +66,6 @@ inline void encode_le_uint64(uint8_t *buf, uint64_t val) {
     *buf++ = val >> 40;
     *buf++ = val >> 48;
     *buf++ = val >> 56;
-}
-
-// big endian binary decoding
-
-inline uint16_t decode_be_uint16(uint8_t *buf) {
-    return ((uint16_t)(buf[0]) << 8) | ((uint16_t)(buf[1]));
-}
-
-inline uint32_t decode_be_uint24(uint8_t *buf) {
-    return (((uint32_t)(buf[0])) << 16) | (((uint32_t)(buf[1])) << 8) | ((uint32_t)(buf[2]));
-}
-
-inline uint32_t decode_be_uint32(uint8_t *buf) {
-    return ((uint32_t)(buf[0]) << 24) | (((uint32_t)(buf[1])) << 16) | (((uint32_t)(buf[2])) << 8) |
-            ((uint32_t)(buf[3]));
-}
-
-inline uint64_t decode_be_uint64(uint8_t *buf) {
-    return ((uint64_t)(buf[0]) << 56) | (((uint64_t)(buf[1])) << 48) | (((uint64_t)(buf[2])) << 40) |
-            (((uint64_t)(buf[3])) << 32) | ((uint64_t)(buf[4]) << 24) | (((uint64_t)(buf[5])) << 16) |
-            (((uint64_t)(buf[6])) << 8) | ((uint64_t)(buf[7]));
 }
 
 // big endian binary encoding
