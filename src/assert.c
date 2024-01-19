63a03ba9271cb7b7e620298e1a6e5d798c83d7d8
Luuk van Dijk <lvd@daedalean.ai>
2024-01-20 00:30:10 +0100

    added assert and started to use in places

---
+++ src/assert.c
@@ -0,0 +1,10 @@
+#include "cortex_m4.h"
+#include "tprintf.h"
+
+#define printf tprintf
+
+void __assert_func (const char * file, int line, const char *func, const char *expr) {
+    printf("\e[K\n%s:%d (%s) assertion failed: %s\e[K\n", file, line, func,expr);
+    for(;;)
+        __BKPT();
+}
